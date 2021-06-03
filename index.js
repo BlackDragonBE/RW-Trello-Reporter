cee34ad752d990d69971453c4d99fa9ef7555356
Eric Van de Kerckhove <BlackDragonBE@users.noreply.github.com>
2021-06-03 09:29:21 +0200

    Another button

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.setHeader('Content-Type', 'text/html');
-  
+
   res.write('<!DOCTYPE html>');
   res.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
   res.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">');
   res.write('<p>Hi there <b>bold</b> one</p>');
   res.write('<div>Whoop</div>');
+  res.write('<a class="button">Button</a>');
   res.write('<a class="button">Button</a>');
   res.end();
 });
